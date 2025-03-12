4dba80cb498656f65793811ec95d2866b3016b22
Cyril Marchive <cyril.marchive@gmail.com>
2025-03-12 17:42:23 +0100

    Use bedrock for foundation model

--- src/services/openai.service.mjs
+++ src/services/openai.service.mjs
@@ -1,23 +1,45 @@
-import { ChatPayload } from "../models/openai.model.mjs";
+import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
 
-export async function callOpenAI(payload) {
-    const apiKey = process.env.OPENAI_API_KEY;
-    const openaiEndpoint = 'https://api.openai.com/v1/chat/completions';
-    const chatPayload = new ChatPayload("gpt-4o-mini", payload.messages);
+const AWS_REGION = process.env.AWS_REGION || "eu-west-3";
+const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0"
 
-    const response = await fetch(openaiEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify(chatPayload),
-    });
+export async function callOpenAI(body) {
+    const client = new BedrockRuntimeClient({ region: AWS_REGION });
+    const systemMessage = body.messages.find(msg => msg.role === "system")?.content || "";
+    const userMessage = body.messages.find(msg => msg.role === "user")?.content || "";
+    const combinedMessage = `${systemMessage}\n\n${userMessage}`;
+    const payload = {
+        anthropic_version: "bedrock-2023-05-31",
+        max_tokens: 50000,
+        temperature: 0.75,
+        messages: [
+            {
+                role: "user",
+                content: [{ type: "text", text: combinedMessage }]
+            }
+        ]
+    };
 
-    if (!response.ok) {
-        const err = await response.text();
-        throw new Error(`Erreur API OpenAI: ${err}`);
-    }
+    const response = await client.send(
+        new InvokeModelCommand({
+            contentType: "application/json",
+            body: JSON.stringify(payload),
+            modelId: MODEL_ID
+        })
+    )
 
-    return await response.json();
+    const decodedResponseBody = new TextDecoder().decode(response.body)
+    const responseBody = JSON.parse(decodedResponseBody)
+    const responses = responseBody.content;
+    const modelResponse = responseBody.content[0]?.text || "";
+
+    return {
+        choices: [
+            {
+                message: {
+                    content: modelResponse
+                }
+            }
+        ]
+    };
 }
